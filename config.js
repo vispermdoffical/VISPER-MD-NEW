591ccf0f65b8aee31a44b2ca1b9ba79ba1dd1b9c
Janidu26 <Janidusandipa26@gmail.com>
2025-07-12 17:24:41 +0530

    Update config.jsname: Node.js CI  on:   push:     branches:       - main   pull_request:     branches:       - main  jobs:   build:     runs-on: ubuntu-latest      strategy:       matrix:         node-version: [20.x]      steps:       - name: Checkout repository         uses: actions/checkout@v3        - name: Set up Node.js         uses: actions/setup-node@v3         with:           node-version: ${{ matrix.node-version }}        - name: Install dependencies         run: npm install         - name: Start application         run: npm start
    name: Node.js CI
    
    on:
      push:
        branches:
          - main
      pull_request:
        branches:
          - main
    
    jobs:
      build:
        runs-on: ubuntu-latest
    
        strategy:
          matrix:
            node-version: [20.x]
    
        steps:
          - name: Checkout repository
            uses: actions/checkout@v3
    
          - name: Set up Node.js
            uses: actions/setup-node@v3
            with:
              node-version: ${{ matrix.node-version }}
    
          - name: Install dependencies
            run: npm install 
    
          - name: Start application
            run: npm start

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
-SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'Janidu' : process.env.SESSION_NAME, 
 
 };
 
